48ebc2028afe1db69b723fd19f0b4e7d81555d40
Oliver Atherton <o.atherton@outlook.com>
2024-09-04 14:15:01 +0100

    feat: started creating functionality on the backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,30 @@
-console.log("backend started");
+// Import dependencies
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+// Initialize Express app
+const app = express();
+dotenv.config();
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
+app.use(helmet());
+app.use(morgan("common"));
+
+//TODO: create backend routes
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err.message));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
